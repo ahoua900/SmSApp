973f13180737aa39d249144b20ade7e8d4b76f3b
Coulibaly Kafalo Alex <ckalexis20@gmail.com>
2020-03-28 00:04:33 +0000

    Ajout des images du TabbedPage

--- AppSmS_Csharp/AppSmS_Csharp.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ AppSmS_Csharp/AppSmS_Csharp.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -63,7 +63,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"456d91d3-e017-4fc1-af07-e33545b18204"
+	.asciz	"dcf42da3-a5bb-4f65-b57b-b138fbf5242f"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
